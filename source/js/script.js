1d7b790d9c13c15a96620398e37f33166b82eb7d
Зайцева Дарья <errorka792@gmail.com>
2018-05-10 15:36:57 +0300

    Финальные правки

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,8 @@ var nav = document.querySelector(".main-nav");
 var navToggle = document.querySelector(".main-nav__toggle");
 
 nav.classList.remove('main-nav--nojs');
+nav.classList.remove('main-nav--opened');
+nav.classList.add('main-nav--closed');
 
 navToggle.addEventListener('click', function () {
 
